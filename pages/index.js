1612fe7c4741f613d1caa8edbbb3b53a0ee78441
Ori Marash <ori@marash.net>
2020-12-08 23:34:12 +0000

    Add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 const IndexPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/ffb31d486cb0f078e7cafbd4c1d1ef4d')
+  })
+
   return (
-    <div></div>
+    <div />
   )
 }
 
